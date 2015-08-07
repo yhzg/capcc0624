f6df6a27018e984fa94768339fbdffc19fd74360
DT <11@126.com>
2015-08-07 14:41:43 +0800

    20150806 后台管理功能更新

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -11,6 +11,7 @@ class IndexController extends Controller {
         }
     }
     public function index(){
+
         $this->display();
     }
 
---
+++ Application/Runtime/Cache/Admin/583dc9525a5f84db1c3ebf44335a7406.php
@@ -0,0 +1,26 @@
+<?php if (!defined('THINK_PATH')) exit();?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>运河网后台管理系统 </title>
+    <script type="text/javascript" src="/capcc0624/Public/Admin/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="/capcc0624/Public/Admin/index.css" />
+    <link rel="stylesheet" type="text/css" href="/capcc0624/Public/Home/Css/1.1home_font.css" />
+</head>
+<body>
+<div id="mainDiv">
+    <div id="topDiv">
+        <div id="tmenu">
+            <ul>
+                <li ><a href="<?php echo U('Index/index');?>">后台首页</a></li>
+                <li ><a href="<?php echo U('News/picture');?>">图说新闻</a></li>
+                <li ><a href="<?php echo U('News/active');?>">动态新闻</a></li>
+                <li ><a href="#">菜单</a></li>
+                <li ><a href="#">菜单</a></li>
+                <li ><a href="#">菜单</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
